fc29421a7e60a87853a42ea3b7a7e77e9fb23e07
Sajna M <sajna.m@difinitydigital.com>
2026-03-13 14:55:41 +0530

    customer

---
+++ src/app/core/service/api-services/customer/customer.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../api-service';
+
+@Injectable({ providedIn: 'root' })
+export class CustomerApiService {
+
+  private url = `customer`;
+
+ constructor(private api: ApiService) {}
+  getAll() {
+    return this.api.get(this.url);
+  }
+ getNextNumber() {
+       return this.api.get(`${this.url}/code`);
+    }
+  getById(id: number) {
+    return this.api.get(`${this.url}/${id}`);
+  }
+
+  create(data: any) {
+    return this.api.post(this.url, data);
+  }
+
+  update(data: any) {
+    return this.api.put(`${this.url}`, data);
+  }
+
+  delete(id: number) {
+    return this.api.delete(`${this.url}/${id}`);
+  }
+   toggleStatus(id: number) {
+      return this.api.delete(`${this.url}/toggle/${id}`);
+    }
+}
